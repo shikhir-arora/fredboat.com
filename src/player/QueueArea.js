183a12be716f6c1ceead0138a386bba24dfcad3c
Frederikam <frogkr@gmail.com>
2018-02-13 17:00:53 +0100

    Create a mock for the queue

--- src/player/QueueArea.js
+++ src/player/QueueArea.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import "./css/Queue.css";
+import QueueItem from "./QueueItem";
+
+const mock = {
+    id: "dQw4w9WgXcQ",
+    title: "Never gonna give you up",
+    source: "youtube",
+    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ", // Nullable
+    image: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg", // Nullable
+    author: "RickAstleyVEVO", // Nullable
+    duration: 3 * 60 + 33, // Nullable if stream
+    startTime: 0 // Unix time
+};
 
 class QueueArea extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            tracks: [mock, mock, mock, mock]
+        };
+    }
+
     render() {
+        const queueItems = [];
+        let i = 0;
+        for (const track in this.state.tracks) {
+            // noinspection JSUnfilteredForInLoop
+            queueItems.push(<QueueItem key={i} track={track}/>);
+            i++;
+        }
+
         return (
             <div className="QueueArea">
+                {queueItems}
             </div>
         )
     }
